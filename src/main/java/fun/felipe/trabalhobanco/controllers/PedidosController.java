e0b7631ed365afc455f71174de2d1870bdbba542
FelipeRos19 <feliperos.simao@gmail.com>
2025-02-18 08:40:35 -0300

    ...

--- src/main/java/fun/felipe/trabalhobanco/controllers/PedidosController.java
+++ src/main/java/fun/felipe/trabalhobanco/controllers/PedidosController.java
@@ -6,25 +6,29 @@ import fun.felipe.trabalhobanco.models.entities.FuncionarioEntity;
 import fun.felipe.trabalhobanco.models.entities.LogEntity;
 import fun.felipe.trabalhobanco.models.entities.PedidoEntity;
 import fun.felipe.trabalhobanco.models.repositories.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController()
 @RequestMapping("/pedidos")
 public class PedidosController {
+    private static final Logger log = LoggerFactory.getLogger(PedidosController.class);
     private final PedidoRepository pedidoRepository;
     private final LogsRepository logsRepository;
     private final ClienteRepository clienteRepository;
     private final FuncionarioRepository funcionarioRepository;
     private final ConfirmacaoEntregaRepository confirmacaoEntregaRepository;
 
-    private final Map<String, String> status = new HashMap<>();
+    private final Map<String, Map<String, String>> status = new HashMap<>();
+    private final List<String> endereco = new ArrayList<>();
 
     public PedidosController(PedidoRepository pedidoRepository, LogsRepository logsRepository, ClienteRepository clienteRepository, FuncionarioRepository funcionarioRepository, ConfirmacaoEntregaRepository confirmacaoEntregaRepository) {
         this.pedidoRepository = pedidoRepository;
@@ -33,11 +37,19 @@ public class PedidosController {
         this.funcionarioRepository = funcionarioRepository;
         this.confirmacaoEntregaRepository = confirmacaoEntregaRepository;
 
-        status.put("Coleta Feita pela Transportadora", "Em Processo");
-        status.put("Saindo de Unidade Logística", "Em Processo");
-        status.put("Chegando a sua Região de Distribuição", "Em Processo");
-        status.put("Em Rota de Entrega", "Em Processo");
-        status.put("Entrega Concluída com Sucesso", "Finalizado");
+        status.put("Coleta Feita pela Transportadora", Map.of("Iniciado", "Rua Lojinha do Seu Zé"));
+        status.put("Entrando na Unidade Logística", Map.of("Em Processo 2", "Rua da Unidade Logística"));
+        status.put("Saindo de Unidade Logística", Map.of("Em Processo 2", "Rua da Unidade Logística"));
+        status.put("Chegando a sua Região de Distribuição", Map.of("Em Processo 3", "Rua da Unidade Logística da sua Região"));
+        status.put("Em Rota de Entrega", Map.of("Em Processo 4", "Rua Perto da sua Casa"));
+        status.put("Entrega Concluída com Sucesso", Map.of("Finalizado", "Rua Obviamente na sua Casa"));
+
+        endereco.add("Rua Lojinha do Seu Zé");
+        endereco.add("Rua da Unidade Logística");
+        endereco.add("Rua da Unidade Logística");
+        endereco.add("Rua da Unidade Logística da sua Região");
+        endereco.add("Rua Perto da sua Casa");
+        endereco.add("Rua Obviamente na sua Casa\"");
     }
 
     @PostMapping("/create")
@@ -53,7 +65,17 @@ public class PedidosController {
 
         PedidoEntity savedPedido = pedidoRepository.save(pedido);
 
-        LogEntity logEntity = new LogEntity();
+        for (int i = 0; i < 6; i++) {
+            LogEntity logEntity = new LogEntity();
+            logEntity.setId(new Random().nextInt(0, 100000000));
+            logEntity.setTimestamp(LocalDateTime.now().plusHours(i));
+            logEntity.setLocation(endereco.get(i));
+            logEntity.setIdOrder(savedPedido.getId());
+            String status = (i != 5) ? "Em processo" : "Finalizado";
+            logEntity.setStatus(status);
+            logEntity.setIdCar(savedPedido.getFuncionario().getId());
+            this.logsRepository.save(logEntity);
+        }
 
         return ResponseEntity.ok(savedPedido);
     }
